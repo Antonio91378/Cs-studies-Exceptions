b5784beae8ff5459b7b90b1233557f59d7b18b06
Antonio Dias <antonio.dias@bit.com.br>
2022-06-14 17:22:04 -0300

    fist ommit

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using ByteBank;
+namespace ByteBank.SistemaAgencia
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1023, 456456456);
+            Console.WriteLine(conta.Numero);
+        }
+    }
+
+}
