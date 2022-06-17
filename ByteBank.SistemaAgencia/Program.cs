f214cb70db2e3a83b5113f9329eb0541d3853a0d
Antonio Dias <antonio.dias@bit.com.br>
2022-06-17 17:17:04 -0300

    usando o nuget

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using ByteBank;
+using Humanizer;
+
 namespace ByteBank.SistemaAgencia
 {
     //qnd n tem nenhum modificador de acesso, minha classe é internal
@@ -8,33 +10,10 @@ namespace ByteBank.SistemaAgencia
         private static void Main(string[] args)
         {
             DateTime dataFimPagamento = new DateTime(2022, 7, 17);
- 
             DateTime dataAtual = DateTime.Now;
             TimeSpan diferenca = dataFimPagamento - dataAtual;
-            string mensagem = GetIntervaloDeTempoLegivel(diferenca);
-            System.Console.WriteLine("Vencimento em : " + mensagem);
-        }
-        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
-        {
-            if (timeSpan.Days > 30)
-            {
-                int quantidadeMeses = timeSpan.Days / 30;
-                int quantidadeDias = timeSpan.Days % 30;
-                if (quantidadeDias > 7)
-                {
-                    int diasRestantes = quantidadeDias % 7;
-                    int quantidadeSemanas = quantidadeDias / 7;
-                    return quantidadeMeses + " meses e " + quantidadeSemanas + " Semanas e " + diasRestantes + " dias.";
-                }
-                return quantidadeMeses + " meses e " + quantidadeDias + " dias.";
-            }
-            if (timeSpan.Days > 7)
-            {
-                int quantidadeSemanas = timeSpan.Days / 7;
-                int quantidadeDias = timeSpan.Days % 7;
-                return quantidadeSemanas + " Semanas e " + quantidadeDias + " dias.";
-            }
-            return timeSpan.Days + " dias";
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);
         }
 
     }
