a7a375d1fe380ec64d3b608676bc170c006dcec2
Antonio Dias <antonio.dias@bit.com.br>
2022-06-17 16:31:52 -0300

    tentando lidar com semanas dias e meses, ademais irei usar a lib humanizr

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -7,15 +7,33 @@ namespace ByteBank.SistemaAgencia
     {
         private static void Main(string[] args)
         {
-            DateTime dataFimPagamento = new DateTime(2022, 7, 17);
+            DateTime dataFimPagamento = new DateTime(2022, 8, 9);
             DateTime dataAtual = DateTime.Now;
             TimeSpan diferenca = dataFimPagamento - dataAtual;
             string mensagem = GetIntervaloDeTempoLegivel(diferenca);
             System.Console.WriteLine("Vencimento em : " + mensagem);
         }
-        static string GetIntervaloDeTempoLegivel(TimeSpan timespan)
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
         {
-            return timespan.Days + " dias";
+            if (timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                int quantidadeDias = timeSpan.Days % 30;
+                if (quantidadeDias > 7)
+                {
+                    int diasRestantes = quantidadeDias % 7;
+                    int quantidadeSemanas = quantidadeDias / 7;
+                    return quantidadeMeses + " meses e "+ quantidadeSemanas + " Semanas e " + diasRestantes + " dias.";
+                }
+                return quantidadeMeses + " meses e " + quantidadeDias + " dias.";
+            }
+            if (timeSpan.Days > 7)
+            {
+                int quantidadeSemanas = timeSpan.Days / 7;
+                int quantidadeDias = timeSpan.Days % 7;
+                return quantidadeSemanas + " Semanas e " + quantidadeDias + " dias.";
+            }
+            return timeSpan.Days + " dias";
         }
     }
 
